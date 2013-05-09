9e4fb5c0418dbc9f5a7c093657013bb7e77c70ce
Adam Gilgoff <adam.gilg@gmail.com>
2013-05-09 14:01:05 -0700

    adding quotes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController < ApplicationController
     @order = Order.new(params[:order])
     @order.user_id = current_user.id
     if @order.save
-      SandwichMailer.send_order(current_user, Sandwich.find(@order.sandwich_id)).deliver
+      SandwichMailer.send_order(current_user, Sandwich.find(@order.sandwich_id), Quote.offset(rand(Quote.count).first).deliver
       flash[:notice] = "Order successfully saved and sent"
       redirect_to root_url
     else
--- app/mailers/sandwich_mailer.rb
+++ app/mailers/sandwich_mailer.rb
@@ -1,9 +1,10 @@
 class SandwichMailer < ActionMailer::Base
   default from: "make@sandwich.com"
 
-  def send_order(current_user, sandwich)
+  def send_order(current_user, sandwich, quote)
     @user = current_user
     @sandwich = sandwich
+    @quote = quote
     mail(to: @user.email, subject: "Sandwich!")
   end
 end
