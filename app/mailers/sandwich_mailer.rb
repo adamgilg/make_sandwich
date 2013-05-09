91b2dcb799472afb4b6a657fd605cff97c6b4aae
Adam Gilgoff <adam.gilg@gmail.com>
2013-05-09 11:38:58 -0700

    renamed folder...

--- app/mailers/sandwich_mailer.rb
+++ app/mailers/sandwich_mailer.rb
@@ -1,9 +1,9 @@
 class SandwichMailer < ActionMailer::Base
   default from: "make@sandwich.com"
 
-  def send_order(current_user)
+  def send_order(current_user, sandwich)
     @user = current_user
-    # @sandwich = sandwich
+    @sandwich = sandwich
     mail(to: @user.email, subject: "Sandwich!")
   end
 end
